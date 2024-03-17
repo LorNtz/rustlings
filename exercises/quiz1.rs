dad3634aceca0dcdd77b2aee5eddb3b86a83b7e9
LorNtz <lortz@foxmail.com>
2024-03-18 00:04:47 +0800

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: u8) -> u8 {
+    let price = if cnt <= 40 {
+        cnt * 2
+    } else {
+        cnt
+    };
+    price
+}
 
 // Don't modify this function!
 #[test]
